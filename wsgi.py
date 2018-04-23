67f304ead114b9dba1b2692dfdb3f6230ce0416f
root <root@ocp-lab.aq.lab>
2018-04-23 08:29:29 +0000

    bla bla bla

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! This is B v7.77 app!! Yay!!!"
+    return "Hello World! This is B v7.77 app!! Hooray!!!"
 
 if __name__ == "__main__":
     application.run()
